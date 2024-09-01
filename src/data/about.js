028211f76990cfb9a9e97fb2195e1a1d4944b0e5
Charles Langlois <charles.langlois.dev@gmail.com>
2024-09-01 11:53:55 +0200

    Feat/data/about.js

---
+++ src/data/about.js
@@ -0,0 +1,24 @@
+const about = [
+  {
+    title: "Fiabilité",
+    about:
+      "Les annonces prostées sur Kasa garantissent une fiablité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+  },
+  {
+    title: "Respect",
+    about:
+      "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+  },
+  {
+    title: "Service",
+    about:
+      "La qualité du service est coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.",
+  },
+  {
+    title: "Sécurité",
+    about:
+      "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pous nos hôtes.",
+  },
+];
+
+export default about;
