9620634e73bbb56be808ed343661121fc0ffb622
Charles Langlois <charles.langlois.dev@gmail.com>
2024-09-04 10:40:27 +0200

    Feat/dossier/router/router.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,5 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import homeBackgroundImg from "../assets/Kasa-1.png";
-import proposBackgroundImg from "../assets/Kasa-2.png";
-
-import Banner from "./Banner";
-import BannerImage from "./BannerImage";
-import Gallery from "./Gallery";
-import About from "./About";
-import Footer from "./Footer";
-
-import logementsList from "../data/logementsList";
-import about from "../data/about";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div className="main-container">
-        <Banner />
-        <main>
-          <BannerImage
-            title="Chez vous, partout et ailleurs"
-            backgroundImage={homeBackgroundImg}
-          />
-          <Gallery logements={logementsList} />
-        </main>
-        <Footer />
-      </div>
-    ),
-  },
-  {
-    path: "/A-Propos",
-    element: (
-      <div className="main-container">
-        <Banner />
-        <main>
-          <BannerImage title="" backgroundImage={proposBackgroundImg} />
-          <About abouts={about} />
-        </main>
-        <Footer />
-      </div>
-    ),
-  },
-  {
-    path: "/Fiche-Logement",
-    element: <div>Fiche Logement</div>,
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import router from "../router/router";
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/router/router.js
@@ -0,0 +1,51 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import homeBackgroundImg from "../assets/Kasa-1.png";
+import proposBackgroundImg from "../assets/Kasa-2.png";
+
+import Banner from "../components/Banner";
+import BannerImage from "../components/BannerImage";
+import Gallery from "../components/Gallery";
+import About from "../components/About";
+import Footer from "../components/Footer";
+
+import logementsList from "../data/logementsList";
+import about from "../data/about";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="main-container">
+        <Banner />
+        <main>
+          <BannerImage
+            title="Chez vous, partout et ailleurs"
+            backgroundImage={homeBackgroundImg}
+          />
+          <Gallery logements={logementsList} />
+        </main>
+        <Footer />
+      </div>
+    ),
+  },
+  {
+    path: "/A-Propos",
+    element: (
+      <div className="main-container">
+        <Banner />
+        <main>
+          <BannerImage title="" backgroundImage={proposBackgroundImg} />
+          <About abouts={about} />
+        </main>
+        <Footer />
+      </div>
+    ),
+  },
+  {
+    path: "/Fiche-Logement",
+    element: <div>Fiche Logement</div>,
+  },
+]);
+
+export default router;
