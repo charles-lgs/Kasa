175ab08e7c9887375d47125811e5d1c9b3bb9057
Charles Langlois <charles.langlois.dev@gmail.com>
2024-09-04 13:42:43 +0200

    Feat/style/NotFound.jsx

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import '../styles/NotFound.scss'
 
 function NotFound() {
     return (
         <div className="not-found">
             <h1>404</h1>
             <h2>Oups! La page que vous demandez n'existe pas.</h2>
-            <Link to='/'>Retourner sur la page d'accueil</Link>
+            <Link to='/' className="link">Retourner sur la page d'accueil</Link>
         </div>
     )
 }
